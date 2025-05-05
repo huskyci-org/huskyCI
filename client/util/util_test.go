f039c8119c4357604c45fd3a37c0ea41d306389f
Guilherme Ferreira <github.onstage427@passmail.net>
2025-05-05 15:51:44 -0300

    chore(client/util): updating io/util package to os package

--- client/util/util_test.go
+++ client/util/util_test.go
@@ -2,7 +2,6 @@ package util_test
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 
 	"github.com/huskyci-org/huskyCI/client/util"
@@ -15,7 +14,7 @@ var _ = Describe("Util", func() {
 		outputFileName := "sonarqube_test.json"
 		outputFilePath := testOutputFilesPath + outputFileName
 		testDataPath := "./testdata/sonarqube/sonarqube_test_example.json"
-		fileString, err := ioutil.ReadFile(testDataPath)
+		fileString, err := os.ReadFile(testDataPath)
 		if err != nil {
 			Fail(fmt.Sprintf("error trying to read fixture file: %s", err.Error()))
 		}
@@ -32,7 +31,7 @@ var _ = Describe("Util", func() {
 			Expect(!os.IsNotExist(err)).To(Equal(true))
 		})
 		It("File content should match the input string", func() {
-			outputString, err := ioutil.ReadFile(outputFilePath)
+			outputString, err := os.ReadFile(outputFilePath)
 			if err != nil {
 				Fail(fmt.Sprintf("error trying to read test output file: %s", err.Error()))
 			}
