285cdf95ecba97ba3afcd5404ab7c713bf4b851b
Guilherme Ferreira <github.onstage427@passmail.net>
2025-05-09 14:56:47 -0300

    feat(scripts): adding trivy and updating dockerhub org

--- deployments/scripts/build-containers.sh
+++ deployments/scripts/build-containers.sh
@@ -3,15 +3,16 @@
 # This script will build every securityTest container based on all dockerfiles from huskyCI repository
 #
 
-docker buildx build --platform linux/amd64 deployments/dockerfiles/bandit/ -t huskyci/bandit:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/brakeman/ -t huskyci/brakeman:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/enry/ -t huskyci/enry:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/gitauthors/ -t huskyci/gitauthors:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/gosec/ -t huskyci/gosec:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/npmaudit/ -t huskyci/npmaudit:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/npmaudit/ -t huskyci/yarnaudit:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/safety/ -t huskyci/safety:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/gitleaks/ -t huskyci/gitleaks:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/spotbugs/ -t huskyci/spotbugs:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/tfsec/ -t huskyci/tfsec:latest
-docker buildx build --platform linux/amd64 deployments/dockerfiles/securitycodescan/ -t huskyci/securitycodescan:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/bandit/ -t huskyciorg/bandit:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/brakeman/ -t huskyciorg/brakeman:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/enry/ -t huskyciorg/enry:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/gitauthors/ -t huskyciorg/gitauthors:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/gosec/ -t huskyciorg/gosec:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/npmaudit/ -t huskyciorg/npmaudit:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/npmaudit/ -t huskyciorg/yarnaudit:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/safety/ -t huskyciorg/safety:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/gitleaks/ -t huskyciorg/gitleaks:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/spotbugs/ -t huskyciorg/spotbugs:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/tfsec/ -t huskyciorg/tfsec:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/trivy/ -t huskyciorg/trivy:latest
+docker buildx build --platform linux/amd64 deployments/dockerfiles/securitycodescan/ -t huskyciorg/securitycodescan:latest
--- deployments/scripts/check-containers-version.sh
+++ deployments/scripts/check-containers-version.sh
@@ -4,18 +4,19 @@
 #
 
 
-banditVersion=$(docker run --rm huskyci/bandit:latest bandit --version | grep bandit | awk -F " " '{print $2}')
-brakemanVersion=$(docker run --rm huskyci/brakeman:latest brakeman --version | awk -F " " '{print $2}')
-enryVersion=$(docker run --rm huskyci/enry:latest enry --version)
-gitAuthorsVersion=$(docker run --rm huskyci/gitauthors:latest git --version | awk -F " " '{print $3}')
-gosecVersion=$(docker run --rm huskyci/gosec:latest gosec --version | grep Version | awk -F " " '{print $2}')
-npmAuditVersion=$(docker run --rm huskyci/npmaudit:latest npm audit --version)
-yarnAuditVersion=$(docker run --rm huskyci/yarnaudit:latest yarn audit --version )
-safetyVersion=$(docker run --rm huskyci/safety:latest safety --version | awk -F " " '{print $3}')
-gitleaksVersion=$(docker run --rm huskyci/gitleaks:latest gitleaks --version)
-spotbugsVersion=$(docker run --rm huskyci/spotbugs:latest cat /opt/spotbugs/version)
-tfsecVersion=$(docker run --rm huskyci/tfsec:latest ./tfsec -v)
-securitycodescanVersion=$(docker run --rm huskyci/securitycodescan:latest security-scan | grep tool | awk -F " " '{print $6}')
+banditVersion=$(docker run --rm huskyciorg/bandit:latest bandit --version | grep bandit | awk -F " " '{print $2}')
+brakemanVersion=$(docker run --rm huskyciorg/brakeman:latest brakeman --version | awk -F " " '{print $2}')
+enryVersion=$(docker run --rm huskyciorg/enry:latest enry --version)
+gitAuthorsVersion=$(docker run --rm huskyciorg/gitauthors:latest git --version | awk -F " " '{print $3}')
+gosecVersion=$(docker run --rm huskyciorg/gosec:latest gosec --version | grep Version | awk -F " " '{print $2}')
+npmAuditVersion=$(docker run --rm huskyciorg/npmaudit:latest npm audit --version)
+yarnAuditVersion=$(docker run --rm huskyciorg/yarnaudit:latest yarn audit --version )
+safetyVersion=$(docker run --rm huskyciorg/safety:latest safety --version | awk -F " " '{print $3}')
+gitleaksVersion=$(docker run --rm huskyciorg/gitleaks:latest gitleaks --version)
+spotbugsVersion=$(docker run --rm huskyciorg/spotbugs:latest cat /opt/spotbugs/version)
+trivyVersion=$(docker run --rm huskyciorg/trivy:latest trivy --version | awk -F " " '{print $3}')
+tfsecVersion=$(docker run --rm huskyciorg/tfsec:latest ./tfsec -v)
+securitycodescanVersion=$(docker run --rm huskyciorg/securitycodescan:latest security-scan | grep tool | awk -F " " '{print $6}')
 
 echo "bandit: $banditVersion"
 echo "brakeman: $brakemanVersion"
