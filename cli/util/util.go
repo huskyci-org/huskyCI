33c59e6d985566a591713e4e948a4f1a880f14a2
Guilherme Ferreira <github.onstage427@passmail.net>
2026-01-26 17:38:33 -0300

    refactor: replace archiver library with standard library zip for security

--- cli/util/util.go
+++ cli/util/util.go
@@ -1,14 +1,16 @@
 package util
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/huskyci-org/huskyCI/cli/config"
 	"github.com/huskyci-org/huskyCI/cli/errorcli"
-	"github.com/mholt/archiver"
 )
 
 // GetAllAllowedFilesAndDirsFromPath returns a list of all files and dirs allowed to be zipped
@@ -25,7 +27,9 @@ func GetAllAllowedFilesAndDirsFromPath(path string) ([]string, error) {
 		if err := checkFileExtension(fileName); err != nil {
 			continue
 		} else {
-			allFilesAndDirNames = append(allFilesAndDirNames, fileName)
+			// Return full path for zip creation
+			fullPath := filepath.Join(path, fileName)
+			allFilesAndDirNames = append(allFilesAndDirNames, fullPath)
 		}
 	}
 
@@ -42,11 +46,82 @@ func CompressFiles(allFilesAndDirNames []string) (string, error) {
 		return fullFilePath, err
 	}
 
-	if err := archiver.Archive(allFilesAndDirNames, fullFilePath); err != nil {
+	// Create zip file using standard library (more secure, no path traversal vulnerability)
+	zipFile, err := os.Create(fullFilePath)
+	if err != nil {
 		return fullFilePath, err
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	// Add each file/directory to the zip
+	for _, filePath := range allFilesAndDirNames {
+		if err := addToZip(zipWriter, filePath); err != nil {
+			return fullFilePath, err
+		}
 	}
 
 	return fullFilePath, nil
+}
+
+// addToZip adds a file or directory to the zip archive
+func addToZip(zipWriter *zip.Writer, filePath string) error {
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		return err
+	}
+
+	if fileInfo.IsDir() {
+		// Recursively add directory contents
+		return filepath.Walk(filePath, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			// Skip the directory itself, only add files
+			if info.IsDir() {
+				return nil
+			}
+
+			// Create relative path for zip entry (prevents path traversal)
+			relPath, err := filepath.Rel(filepath.Dir(filePath), path)
+			if err != nil {
+				return err
+			}
+
+			// Sanitize path to prevent path traversal
+			relPath = filepath.ToSlash(relPath)
+			// Check for path traversal attempts
+			if filepath.IsAbs(relPath) || relPath == ".." || strings.HasPrefix(relPath, "../") {
+				return fmt.Errorf("illegal file path: %s", relPath)
+			}
+
+			return addFileToZip(zipWriter, path, relPath)
+		})
+	}
+
+	// Add single file
+	relPath := filepath.Base(filePath)
+	return addFileToZip(zipWriter, filePath, relPath)
+}
+
+// addFileToZip adds a single file to the zip archive
+func addFileToZip(zipWriter *zip.Writer, filePath, zipPath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer, err := zipWriter.Create(zipPath)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(writer, file)
+	return err
 }
 
 // GetZipFriendlySize returns the size of a friendly zip file size based on its destination
